b0bd804cf66d35a709b9ed57ee3bd12d02c3e03c
Lei <kanlei@live.com>
2013-11-15 18:26:07 +0800

    Update Singleton.cs
    Add description about 'beforefieldinit' and 'sealed'

--- Singleton.cs
+++ Singleton.cs
@@ -1,8 +1,9 @@
 // 1.第一种方式
-public class Singleton
+public sealed class Singleton
 {
     // 静态在变量或属性被第一次访问之前执行，在静态构造函数执行之前执行
-    private static readonly Singleton singleton=new Singleton();
+    // 可能会到导致提前运行，由于BeforeFieldInit
+    private  static readonly Singleton singleton=new Singleton();
 
     public static Singleton TheSingleton
     {
@@ -13,11 +14,12 @@ public class Singleton
 }
 
 // 2.第二种方式
-public class Singleton
+public sealed class Singleton
 {
     // 静态构造函数在变量或属性被第一次访问之前执行
     private static readonly Singleton singleton;
 
+    // 显示地告诉编译器不被标记为BeforeFieldInit
     static Singleton()
     {
         singleton = new Singleton();
