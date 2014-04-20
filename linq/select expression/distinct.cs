571703d06e4f5d7747ec0204e601fceac18e30fb
Lei <kanlei@live.com>
2014-04-20 22:49:02 +0800

    add distinct.cs to linq folder

---
+++ linq/select expression/distinct.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * 去除重复项
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IEnumerable<string> enumerableMethodNames = (from method in typeof(Enumerable).GetMembers(
+                                                        System.Reflection.BindingFlags.Static |
+                                                        System.Reflection.BindingFlags.Public)
+                                                     select method.Name).Distinct();
+
+        foreach (string method in enumerableMethodNames)
+        {
+            Console.Write(" {0}", method);
+        }
+
+
+        Console.Read();
+    }
+}
