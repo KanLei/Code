62f9e9b185e924c9e5e59965b3bceccdb572f826
Lei <kanlei@live.com>
2014-09-20 15:35:23 +0800

    add constructor method with parameter

--- Factory.cs
+++ Factory.cs
@@ -4,4 +4,10 @@ public class Factory
 	{
 		return new T();
 	}
+
+	// 有参构造函数
+	public static T CreateInstance<T>(int num)
+        {
+            return (T)Activator.CreateInstance(typeof(T), num);
+        }
 }
