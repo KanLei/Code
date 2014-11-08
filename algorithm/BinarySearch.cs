dafa6b7e654575cf44709a51f3414bb048a82c9f
Lei <kanlei@live.com>
2014-11-08 20:21:04 +0800

    add new code file

---
+++ algorithm/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿
+class Search
+{
+    /// <summary>
+    /// 折半查找
+    /// </summary>
+    /// <param name="key">要查找的 key</param>
+    /// <param name="array">有序数组</param>
+    /// <returns>查找成功返回下标位置，否则返回 null</returns>
+    static int? BinarySearch(char key, char[] array)
+    {
+        int low = 0;
+        int high = array.Length - 1;
+
+        while (low <= high)
+        {
+            int middle = (low + high) / 2;
+            int value = array[middle];
+
+            if (value < key)
+            {
+                low = middle + 1;
+            }
+            else if (value > key)
+            {
+                high = middle - 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+        return null;
+    }
+}
+
---
+++ algorithm/LinkList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Node
+{
+    public Node m_next;
+    public string value;
+
+    public Node(string name)
+    {
+        this.value = name;
+    }
+}
+
+public sealed class LinkedList
+{
+    public Node m_head;
+
+    public LinkedList Add(Node newNode)
+    {
+        // 头插法
+        newNode.m_next = m_head;
+        m_head = newNode;
+        return this;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var linkList = new LinkedList();
+        //linkList.Add(new Node("Node1"));
+        //linkList.Add(new Node("Node2"));
+        //linkList.Add(new Node("Node3"));
+        //linkList.Add(new Node("Node4"));
+
+        linkList.Add(new Node("Apple")).Add(new Node("Orange")).
+            Add(new Node("Banana")).Add(new Node("Pear"));
+
+        var temp = linkList.m_head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.value);
+            temp = temp.m_next;
+        }
+    }
+}
