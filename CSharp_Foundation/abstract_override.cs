877e454a1fd81145535e39fe22eb5d4bca68e325
kanleilei <kanleilei@sodao.com>
2016-04-29 15:00:21 +0800

    add abstract override

---
+++ CSharp_Foundation/abstract_override.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AbstractWithOverride
+{
+    public abstract class A
+    {
+        public virtual string Name{ get { return ""; } }
+    }
+
+    public abstract class B:A
+    {
+        // 强制 C 重新实现 Name，而不是使用 A 提供的默认实现
+        public abstract override string Name{ get; }
+    }
+
+    public class C:B
+    {
+        public override string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
+
